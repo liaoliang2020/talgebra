3fe96d18feb5ba3833356cdefdeb668839aafda6
Liang Liao <liaoliang2018@gmail.com>
2021-01-06 07:59:36 +0800

    code for generalized scalars

--- tscalar/treal.m
+++ tscalar/treal.m
@@ -1,10 +1,21 @@
-function myreal = timag(tscalar)
-	% this function compute the real part of a tscalar
+function mytreal = treal(tscalar)
 	% checked
-	% This function is optimzied by liaoliang on 11-22-2018
-	% this function is optimized by liaoliang on 11-22-2018 for the second time.
+	% this function returns the real part of a t-scalar
+	% this function is optimzied on 11-22-2018 by liaoliang
+	% this function is optimzied on 11-22-2018 by liaoliang for the second time
 
-	
-	myreal = ifftn(real(fftn(tscalar)));
+	% mytreal = 0.5 * (tscalar + tconj(tscalar));
 
+	mytreal = ifftn(real(fftn(tscalar)));
 end
+
+
+%-------------------------------------------------
+% This following is an equivalent function of the above function, but more computational expensive 
+% function mytreal = treal_proc(tscalar)
+% 	% this function returns the real part of a t-scalar
+% 	[mynorm, myangle] = tnorm_angle(tscalar);
+
+% 	mytreal = tcos(myangle);
+% 	mytreal = tproduct(mytreal, mynorm);
+% end

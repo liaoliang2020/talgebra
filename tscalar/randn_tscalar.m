af208180f372a46616524930773e84283c82c51f
Liang Liao <liaoliang2018@gmail.com>
2021-01-06 12:09:42 +0800

    documentation

---
+++ tscalar/randn_tscalar.m
@@ -0,0 +1,18 @@
+function random_tscalar = randn_tscalar(tsize, mode)
+	assert(isequal(tsize', tsize(:)));
+
+	if nargin == 1
+		mode = 'complex';
+	end
+
+	assert(isequal(mode, 'complex') |  isequal(mode, 'real'));
+	
+	if isequal(mode, 'complex')
+		random_tscalar = randn(tsize) + i * randn(tsize);
+	else 
+		assert(isequal(mode, 'real'));
+		random_tscalar = randn(tsize);
+	end
+	
+	
+end
--- tscalar/tpower.m
+++ tscalar/tpower.m
@@ -1,5 +1,9 @@
 function myresult = tpower(tsclar, p_value)
-	% this function compute the square root of a t-scalar
+	% this function can compute the p power of a t-scalar
+	% this function computes the square root of a t-scalar when p_value is equal to 0.5
+	assert(isnumeric(p_value));
+	assert(numel(p_value) == 1);
+	
 	myresult =  ifftn(power(fftn(tsclar), p_value));
 
 end

1853150856edebd0e025cfbc8c77b53c740746e6
Liang Liao <liaoliang2018@gmail.com>
2021-01-06 12:15:50 +0800

    code for generalized scalars

--- tscalar/tpower.m
+++ tscalar/tpower.m
@@ -1,4 +1,5 @@
 function myresult = tpower(tsclar, p_value)
+	
 	% this function can compute the p power of a t-scalar
 	% this function computes the square root of a t-scalar when p_value is equal to 0.5
 	assert(isnumeric(p_value));

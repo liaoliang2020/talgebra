95ffd7824363e6af6a3ea50f07e83ad2824bf30e
Liang Liao <liaoliang2018@gmail.com>
2021-01-09 18:05:28 +0800

    code for generalized matrices

--- tmatrix/tabs.m
+++ tmatrix/tabs.m
@@ -1,5 +1,4 @@
 function result = tabs(tmatrix, tsize)
-
 	% checked
 	% this function computes the (generalized) absolute value of each t-scalar entity of a t-matrix.  
 	assert(isequal(tsize', tsize(:)));	  

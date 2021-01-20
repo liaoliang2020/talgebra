7a0e087161783c40084d68265ec715e64178881d
Liang Liao <liaoliang2018@gmail.com>
2021-01-20 16:00:29 +0800

    code for generalized matrices

--- tmatrix/ttrace.m
+++ tmatrix/ttrace.m
@@ -1,9 +1,10 @@
 function tresult = ttrace(tmatrix, tsize)
 	% checked	
+	% this function generalizes the trace of a canonical matrix
 
 	assert(isequal(tsize', tsize(:)) );	  
 	assert(ndims(tmatrix) - numel(tsize) == 2 |  ndims(tmatrix) - numel(tsize) == 0)
-	assert(size(tmatrix, numel(tsize) + 1) == size(tmatrix, numel(tsize) + 2));
+	assert(size(tmatrix, numel(tsize) + 1) == size(tmatrix, numel(tsize) + 2));  %input t-matrix must be square. 
 
 	
 	tresult = tdiag(tmatrix, tsize);

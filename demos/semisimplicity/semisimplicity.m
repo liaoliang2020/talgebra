074cc8d3cb6de6b962be8b1fd06d12efa43a4935
Liang Liao <liaoliang2018@gmail.com>
2021-01-22 17:43:55 +0800

    updated on 20210122

---
+++ demos/semisimplicity/semisimplicity.m
@@ -0,0 +1,37 @@
+function semisimplicity
+	% This verifies the idempotent decomposition (also known direct product decomposition) 
+	% is equal to the Fourier transform 
+
+	clear; close all; clc;
+
+	tsize = [3, 3];
+	X = randn(tsize) + i * randn(tsize);
+
+	idem_base = idempotent_base(tsize);
+
+	Y = inv(idem_base) * X(:);
+	Y = reshape(Y, tsize);	
+
+	assert(norm(reshape(fftn(X) - Y, 1, [])) < 1e-6 );
+
+	Y2 = prod(tsize) * idem_base' * X(:);
+	Y2 = reshape(Y2, tsize);
+
+	assert(norm(reshape(Y2 - Y, 1, [])) < 1e-6 );
+
+	%-----------------------------
+	row_num = 3; col_num = 7;
+
+	A = randn([tsize, row_num, col_num]) + i * randn([tsize, row_num, col_num]);
+	B = A;
+	for k = 1: numel(tsize)
+		B = fft(B, [], k);
+	end
+
+	C = prod(tsize) * idem_base' * reshape(A, prod(tsize), []);
+	C = reshape(C, [tsize, row_num, col_num]);
+
+	assert(norm(C(:) - B(:)) < 1e-6 );
+
+
+end

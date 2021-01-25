c2b72d97e366c4f09b66a34c65947321735c5561
Liang Liao <liaoliang2018@gmail.com>
2021-01-25 19:32:12 +0800

    updated on 20210125

--- demos/TPCA/TPCA.m
+++ demos/TPCA/TPCA.m
@@ -7,7 +7,25 @@ function TPCA
 
 	tmatrix = randn([tsize, row_num, col_num]);
 
-	myrank = trank(tmatrix, tsize)
+	myrank = trank(tmatrix, tsize);
+	tvector_mean = tmean(tmatrix, 2, tsize);
+	
+	
+	tmatrix_without_mean = tmatrix - trepmat(tvector_mean, [1, col_num], tsize);
+	myrank = trank(tmatrix_without_mean, tsize);
+
+	[TU, TS, ~] = tsvd(tmatrix_without_mean, tsize);
+	% TU = tmultiplication_arg3(TU, TS, tpinv(TS, tsize), tsize);
+
+	G = tcov(tmatrix_without_mean, tsize);
+	[TU2, TS, ~] = tsvd(G, tsize);
+	TU2 = TU2(:, :, :, 4);
+	TU = TU(:, :, :, 4);
+
+	liaoliang2 = tmultiplication(TU2, tctranspose(TU2, tsize), tsize);
+	liaoliang  = tmultiplication(TU,  tctranspose(TU, tsize),  tsize);
+
+	isequal(round(liaoliang2, 4), round(liaoliang, 4))  
 
 
 end

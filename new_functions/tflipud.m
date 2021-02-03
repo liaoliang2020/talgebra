1df1e0fe4e279c111363365fd22cf5e83b561aa9
liaoliang <liaoliang2018@gmail.com>
2021-02-03 21:14:31 +0800

    updated on 20210203

--- new_functions/tflipud.m
+++ new_functions/tflipud.m
@@ -1,5 +1,5 @@
-function result = tfliplr(tmatrix, tsize)
-	% This function generates the matlab function fliplr
+function result = tflipud(tmatrix, tsize)
+	% This function generates the matlab function flipud
 	assert(isequal(tsize', tsize(:)));	  
 	assert(ndims(tmatrix) - numel(tsize) == 2 | ndims(tmatrix) - numel(tsize) == 1| ndims(tmatrix) - numel(tsize) == 0);
 
@@ -7,8 +7,6 @@ function result = tfliplr(tmatrix, tsize)
 	col_num = size(tmatrix, numel(tsize) + 2);
 
 	tmatrix = reshape(tmatrix, [prod(tsize), row_num, col_num]);
-	result = tmatrix(:, :, col_num:-1:1);
-	result = reshape(result, [tsize, row_num, col_num]); 
-	
-
+	result = tmatrix(:, row_num:-1:1, :);
+	result = reshape(result, [tsize, row_num, col_num]); 	
 end

ee44475cb4c9d7660d0f583c97bcfd1ce3ccea18
liaoliang <liaoliang2018@gmail.com>
2021-02-05 18:11:59 +0800

    updated on 20210205

--- demos/manifold/Stiefel_manifold.m
+++ demos/manifold/Stiefel_manifold.m
@@ -3,13 +3,15 @@ function Stiefel_manifold
 	row_num = 11;
 	col_num = 3;
 
-	X = orth(randn(row_num, col_num) + i * randn(row_num, col_num) );
-	Z = randn(row_num, col_num) + i * randn(row_num, col_num);
-
+	X = orth(randn(row_num, col_num) + 0 * randn(row_num, col_num) );
+	Z = randn(row_num, col_num) + 0 * randn(row_num, col_num);
+	
 	N = X * mysym(ctranspose(X) * Z);
 	T = X * myskew(ctranspose(X)  * Z) + (eye(row_num) - X * ctranspose(X)) * Z;
-	inner_product = trace(T' * N);
-	
+	% inner_product = trace(T' * N)
+
+	inner_product = trace(T' *  (eye(size(X * X')) - 0.5 * X * X') * N)
+
 	assert(isequal(round(N + T, 4), round(Z, 4)));
 	assert(norm(X' * T + T' * X, 'fro') < 1e-8)	
 	assert(abs(inner_product) < 1e-6);

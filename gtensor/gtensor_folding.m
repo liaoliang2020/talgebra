ce596a26dda7f664808462dbf54a6a3e83093cb3
liaoliang <liaoliang2018@gmail.com>
2021-05-08 18:00:11 +0800

    updated on 20210508

--- gtensor/gtensor_folding.m
+++ gtensor/gtensor_folding.m
@@ -1,19 +1,33 @@
-function gtensor = gtensor_folding(tmatrix, mod_k, tsize, gsize)
+function gtensor = gtensor_folding(tmatrix, gsize, mod_k, tsize)
 	% this function is the inverse functio of FUNCTION gtensor_unfolding
 	assert(isequal(tsize', tsize(:)));	 
-	assert(isequal(msize', msize(:)));	 
+	assert(isequal(gsize', gsize(:)));
+	assert(ismember(mod_k, 1: ndims(gsize) ) ); 
+	assert(ndims(tmatrix) - numel(tsize) == 2 | ndims(tmatrix) - numel(tsize) == 1| ndims(tmatrix) - numel(tsize) == 0);
 
-	% msize_tmatrix = size(tmatrix);
-	% msize_tmatrix = msize((numel(tsize) + 1): end  );
-	% assert(~isempty(msize_tmatrix));
+	row_num = size(tmatrix, ndims(tsize) + 1);
+	col_num = size(tmatrix, ndims(tsize) + 2);
+	
+	for i = 1: ndims(tsize)
+		tmatrix = fft(tmatrix, [], i);
+	end
+	
+	tmatrix = reshape(tmatrix, prod(tsize), []);
 
-	% if numel(msize_tmatrix) == 1
-	% 	msize_tmatrix = [msize_tmatrix, 1];
-	% end
+	gtensor = [];
+	for i = 1: prod(tsize)
+		standard_matrix = reshape(tmatrix(i, :), row_num, col_num);
+		tensor = tensorfactork_unflattening(standard_matrix, gsize, mod_k);
 
-	% assert(prod(msize_tmatrix) == prod(gsize));
+		if i == 1
+			gtensor = zeros(prod(tsize), numel(tensor) );
+		end	
+		gtensor(i, :) = reshape(tensor, 1, []);
+	end
 
-	% tmatrix = reshape(tmatrix, ); 
+	gtensor = reshape(gtensor, [tsize, gsize]);
 
-
+	for i = 1: numel(tsize)
+		gtensor = ifft(gtensor, [], i);
+	end
 end

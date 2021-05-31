3b57153323433f7ba82e30b55f68320c5004cbfd
liaoliang <liaoliang2018@gmail.com>
2021-05-31 20:10:25 +0800

    updated on 20210531

---
+++ exp_labs/tscalar/private/get_transforms.m
@@ -0,0 +1,20 @@
+function [trans_matrix, pinv_trans_matrix] = get_transforms(tsize, ftsize)
+	
+	assert(isequal(tsize', tsize(:)));
+	assert(isequal(ftsize', ftsize(:)));
+
+	assert(isequal(size(tsize), size(ftsize) ));
+	assert(all(ftsize - tsize >= 0));
+
+	for i = 1: numel(tsize)
+		A = fourier_matrix(ftsize(i) );
+		A = fliplr(A);	
+		A = A(:, 1: tsize(i));
+		pinvA = pinv(A);
+
+		trans_matrix{i} = A;
+		pinv_trans_matrix{i} = pinvA;
+	end
+	
+
+end

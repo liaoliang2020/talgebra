c9e46b6621e9a9b66c68e14a4ce8bc2410905470
Liang Liao <liaoliang2018@gmail.com>
2022-02-14 18:10:13 +0800

    updated on 02/13/2022

---
+++ quaternion_matrix/qmatrix_operations/qmatrix_inv.m
@@ -0,0 +1,13 @@
+function result = qmatrix_inv(qmatrix)
+	% this function computes the inverse of a square quaternion matrix 
+
+	assert(isequal(class(qmatrix), 'quaternion') );
+	assert(numel(size(qmatrix)) == 2);
+	assert(size(qmatrix, 1) == size(qmatrix, 2));
+
+	matrix = qmatrix2matrix(qmatrix)
+	
+	assert(min(size(matrix)) = rank(matrix));
+	result = matrix2qmatrix(inv(matrix)); 
+
+end

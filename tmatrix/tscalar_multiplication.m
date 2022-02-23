30f0070fbd36b44c0996eac654eefcd42839213f
liaoliang <liaoliangis@126.com>
2022-02-23 09:32:35 +0800

    2022/02/23

--- tmatrix/tscalar_multiplication.m
+++ tmatrix/tscalar_multiplication.m
@@ -1,17 +1,19 @@
 function tmatrix_result = tscalar_multiplication(tscalar, tmatrix, tsize)
+	%This function computes the result of a tscalar multiplified with a tmatrix 
+
 	assert(isequal(tsize', tsize(:)));	  
 	assert(isequal(size(tscalar), tsize));
 	assert(ndims(tmatrix) - numel(tsize) == 2 | ndims(tmatrix) - numel(tsize) == 1| ndims(tmatrix) - numel(tsize) == 0);
 
-	row_num = size(tmatrix_result, numel(tsize) + 1);
-	col_num = size(tmatrix_result, numel(tsize) + 2);
+	row_num = size(tmatrix, numel(tsize) + 1);
+	col_num = size(tmatrix, numel(tsize) + 2);
 	
 	tscalar = fftn(tscalar);
+	tscalar = tscalar(:);
 	for i = 1: numel(tsize)
 		tmatrix = fft(tmatrix, [], i);
 	end
 
-	tscalar = tscalar(:);
 	tmatrix = reshape(tmatrix, prod(tsize), []);
 	tmatrix_result = zeros(size(tmatrix) );
 
@@ -22,7 +24,7 @@ function tmatrix_result = tscalar_multiplication(tscalar, tmatrix, tsize)
 	tmatrix_result = reshape(tmatrix_result, [tsize, row_num, col_num]);	
 
 	for i = 1: numel(tsize)
-		tmatrix_result = fft(tmatrix_result, [], i);
+		tmatrix_result = ifft(tmatrix_result, [], i);
 	end
 
 end

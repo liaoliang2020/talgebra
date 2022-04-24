fa4ff394d4428c0a5b93538facacbea84b7b897a
liaoliang <liaoliangis@126.com>
2022-04-24 21:33:15 +0800

    updated on 04/24/2022

--- canonical_matrix/qfourier_matrix.m
+++ canonical_matrix/qfourier_matrix.m
@@ -1,32 +1,16 @@
-function fourier_matrix_result = qfourier_matrix(n)
+function qfourier_matrix_result = qfourier_matrix(n)
 	% this function computes the nxn Fourier matrix 
 	assert(isscalar(n));
 	assert(n > 0);
 	
-	
-	fourier_matrix_result = fft(eye(n), [], 1);
-
-	fourier_matrix_result = [real(fourier_matrix_result(:)), imag(fourier_matrix_result(:)), imag(fourier_matrix_result(:)), imag(fourier_matrix_result(:))];
-	fourier_matrix_result = quaternion(fourier_matrix_result);
-	fourier_matrix_result = reshape(fourier_matrix_result, [n, n]);
-
-	
-	%---------------------------
-	%the following code is correct, but not efficient.
-	%
-	%fourier_matrix_result = zeros(n, n);
-    %
-	%const = -2 * pi * sqrt(-1) / n;
-	%for i = 1: n
-	%	for j = 1: n
-	%		liaoliang = const * (i - 1) * (j - 1);			
-	%		fourier_matrix_result(i, j) = exp(liaoliang);			
-	%	end
-	%end
-	%-------------------------
-	
-	
-	
-
+	qfourier_matrix_result = quaternionize(zeros(n, n));
+    
+	const = (-2 * pi / n) *  quaternion([0, 1, 1, 1]);
+	for i = 1: n
+		for j = 1: n
+			liaoliang = const * (i - 1) * (j - 1);			
+			qfourier_matrix_result(i, j) = exp(liaoliang);			
+		end
+	end
 	
 end

4333a391ae7399a81c347398c919b088a8e424bc
liaoliang <liaoliangis@126.com>
2022-04-24 23:36:16 +0800

    updated on 04/24/2022

--- canonical_matrix/qfourier_matrix.m
+++ canonical_matrix/qfourier_matrix.m
@@ -1,16 +1,16 @@
 function qfourier_matrix_result = qfourier_matrix(n)
-	% this function computes the nxn Fourier matrix 
+	% this function computes the nxn quaternionic Fourier matrix 
 	assert(isscalar(n));
 	assert(n > 0);
 	
 	qfourier_matrix_result = quaternionize(zeros(n, n));
-    
-	const = (-2 * pi / n) *  quaternion([0, 1, 1, 1]);
-	for i = 1: n
-		for j = 1: n
-			liaoliang = const * (i - 1) * (j - 1);			
-			qfourier_matrix_result(i, j) = exp(liaoliang);			
-		end
-	end
+    qfourier_matrix_result2 = quaternionize(zeros(n, n));	
+
+	const = (-2 * pi / n) * (1 / sqrt(3)) * quaternion([0, 1, 1, 1]);
+	const = repmat(const, n, n);
 	
+	row_vector = 0: (n - 1);
+	qfourier_matrix_result = repmat(row_vector(:), 1, n) * diag(row_vector);
+	qfourier_matrix_result = exp(const .* qfourier_matrix_result);
+
 end

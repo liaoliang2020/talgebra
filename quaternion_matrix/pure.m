f8844d3a0afe71fda599f9b44e17cbf4978a07a4
liaoliang <liaoliangis@126.com>
2022-04-24 08:33:47 +0800

    updated on 04/24/2022

---
+++ quaternion_matrix/pure.m
@@ -0,0 +1,16 @@
+function pure_quaternion_unit = pure(ijk)
+	assert(ijk == 'i' || ijk == 'j' || ijk == 'k');
+
+	switch ijk
+		case 'i'
+			pure_quaternion_unit = quaternion([0 1 0 0]);
+		case 'j'
+			pure_quaternion_unit = quaternion([0 0 1 0]);
+		case 'k'
+			pure_quaternion_unit = quaternion([0 0 0 1]);
+		otherwise
+			assert(false);
+	end
+
+	
+end

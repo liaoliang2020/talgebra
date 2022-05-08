387c6b0a31527e33e09a068d9b0169cb14b573a1
Liang Liao <liaoliang2018@gmail.com>
2022-05-09 02:55:38 +0800

    updated on 05/09/2022

--- tmatrix/tifft.m
+++ tmatrix/tifft.m
@@ -2,6 +2,10 @@ function tmatrix_result = tifft(tmatrix, tsize)
 	assert(isequal(tsize', tsize(:)));	  
 	assert(ndims(tmatrix) - numel(tsize) == 2 |  ndims(tmatrix) - numel(tsize) == 1 | ndims(tmatrix) - numel(tsize) == 0);
 
+	assert(isequal(size(tmatrix), [tsize, size(tmatrix, numel(tsize) + 1), size(tmatrix, numel(tsize) + 2)]) | ...  
+		isequal(size(tmatrix), [tsize, size(tmatrix, numel(tsize) + 1) ]) | ... 
+		isequal(size(tmatrix), tsize) ...
+	); 
 	
 
 	for i = 1: numel(tsize)

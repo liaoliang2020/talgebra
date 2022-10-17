96dd14ec5571b75020074f22952ff89d8a4c46a6
liaoliang <liaoliangis@126.com>
2022-10-17 08:56:58 +0800

    updated on 10/17/2022

--- quaternion_matrix/qarray2carray.m
+++ quaternion_matrix/qarray2carray.m
@@ -1,6 +1,10 @@
-function qarray2carray
+function commplex_array = qarray2carray(qarray)
 	% this function convert a quaternion array to a complex array 
 	% by dropping the j and k parts of each quaternions
 
-	
+	assert(isequal(class(qarray), 'quaternion'));
+	mysize = size(qarray);
+	qarray = comapct(qarray);	
+	qarray = qarray(:, 1) + sqrt(-1) * qarray(:, 2);
+	commplex_array = reshape(qarray, mysize);
 end

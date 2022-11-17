aecd8b9e682c3bee2bbe58341c704adef984a71d
liaoliang <liaoliangis@126.com>
2022-11-17 19:19:04 +0800

    updated on 11/17/2022

--- canonical_matrix/svd_mini.m
+++ canonical_matrix/svd_mini.m
@@ -1,9 +1,16 @@
 function [U, S, V] = svd_min(matrix)
 	assert(isequal(class(matrix), 'double'));
 	assert(ndims(matrix) == 2);
+	
+	r = rank(matrix);
+	if r == 0
+		U = zeros(size(matrix, 1), 1);
+		V = zeros(size(matrix, 2), 1); 
+		S = 0; 
+		return;
+	end
 
 	[U, S, V] = svd(matrix, 'econ');
-	r = rank(matrix);
 	U = U(:, 1:r);
 	V = V(:, 1:r);
 	S = S(1:r, 1:r);

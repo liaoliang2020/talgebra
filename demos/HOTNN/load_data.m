27a8499314b0bf0e79e6520994586288a6fa707e
Liang Liao <76431036+liaoliang2020@users.noreply.github.com>
2023-10-01 18:59:41 +0800

    Delete demos/HOTNN directory

--- demos/HOTNN/load_data.m
+++
@@ -1,8 +0,0 @@
-function data = load_data(path, data_name)
-	assert(ischar(path) & ischar(data_name));
-	file_name_full = sprintf('%s\\%s', path, data_name);
-	data = load(file_name_full);
-	data = struct2cell(data);
-	data = data{1};
-	
-end
